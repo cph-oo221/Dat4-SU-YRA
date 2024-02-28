f5a4c10a50a41f8ff96e4f8682a76cc388f724ad
kotteletfisk <ostekugle@gmail.com>
2024-02-28 14:51:35 +0100

    test bla

--- prototype/src/App.jsx
+++ prototype/src/App.jsx
@@ -1,5 +1,22 @@
 import { useState } from "react";
 import "./App.css";
+import { exec } from "child_process";
+
+function execCommand() {
+  command = 'ls -l';
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing command: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Command stderr: ${stderr}`);
+      return;
+    }
+    console.log(`Command stdout: ${stdout}`);
+  });
+}
 
 function App() {
   // src for tts:  https://dev.to/shantanu_jana/text-to-speech-converter-with-javascript-30oo
@@ -26,6 +43,7 @@ function App() {
         <button id="speak-button" onClick={speak}>
           Speak
         </button>
+        {/* <button onClick={execCommand}>TEST</button> */}
       </div>
     </>
   );
